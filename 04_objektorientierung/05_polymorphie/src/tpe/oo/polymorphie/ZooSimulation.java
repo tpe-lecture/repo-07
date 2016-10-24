b855386cac567537fbf139b87083d51cbca621f3
David <coregaer89@hotmail.de>
2016-10-24 14:50:05 +0200

    Besserer Zoo.

--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
@@ -3,6 +3,7 @@ package tpe.oo.polymorphie;
 import tpe.oo.polymorphie.tiere.Affe;
 import tpe.oo.polymorphie.tiere.Giraffe;
 import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Simulationsklasse.
@@ -15,29 +16,34 @@ public final class ZooSimulation {
     private ZooSimulation() {
         // keine Objekte benötigt
     }
-    
+
     /**
      * Main-Methode.
-     * 
+     *
      * @param args Kommandozeilen-Argumente.
      */
     public static void main(String[] args) {
         Futterstelle futterstelle = new Futterstelle();
-        
+
         Affe charlie = new Affe("Charlie");
         Gorilla buck = new Gorilla("Buck");
         Giraffe debbie = new Giraffe("Debbie");
-        
+
+        ZooTier[] tiere = {charlie, buck, debbie};
+
         System.out.println(charlie);
         System.out.println(buck);
         System.out.println(debbie);
-        
+
         System.out.println("Fütterung...");
-        
-        futterstelle.gibFutter(charlie);
-        futterstelle.gibFutter(buck);
-        futterstelle.gibFutter(debbie);
-        
+
+        for(int i = 0; i < tiere.length; i++){
+            futterstelle.gibFutter(tiere[i]);
+        }
+//        futterstelle.gibFutter(charlie);
+//        futterstelle.gibFutter(buck);
+//        futterstelle.gibFutter(debbie);
+
         System.out.println(charlie);
         System.out.println(buck);
         System.out.println(debbie);
