3bb584b023a8f341e6c8cedc1edce1500d95bc8a
David <coregaer89@hotmail.de>
2016-10-31 11:05:29 +0100

    Nothing changed. o.O

--- 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Geometer.java
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Geometer.java
@@ -9,38 +9,39 @@ public final class Geometer {
     private Geometer() {
         // keine Objekte
     }
-    
+
     /**
      * Hauptmethode.
-     * 
+     *
      * @param args Kommandozeilenargumente
      */
     public static void main(String[] args) {
-        Kreis[] kreise = new Kreis[5];
-        Dreieck[] dreiecke = new Dreieck[5];
-        
-        kreise[0] = new Kreis(10.0);
-        kreise[1] = new Kreis(5.5);
-        kreise[2] = new Kreis(3.3);
-        kreise[3] = new Kreis(7.2);
-        kreise[4] = new Kreis(8.0);
-        
-        dreiecke[0] = new Dreieck(4.0, 3.0);
-        dreiecke[1] = new Dreieck(2.0, 1.0);
-        dreiecke[2] = new Dreieck(5.0, 8.0);
-        dreiecke[3] = new Dreieck(2.0, 7.2);
-        dreiecke[4] = new Dreieck(14.0, 3.8);
-        
+//        Kreis[] kreise = new Kreis[5];
+//        Dreieck[] dreiecke = new Dreieck[5];
+        Figur[] dreieckUndKreise = new Figur[10];
+
+        dreieckUndKreise[0] = new Kreis(10.0);
+        dreieckUndKreise[1] = new Kreis(5.5);
+        dreieckUndKreise[2] = new Kreis(3.3);
+        dreieckUndKreise[3] = new Kreis(7.2);
+        dreieckUndKreise[4] = new Kreis(8.0);
+
+        dreieckUndKreise[5] = new Dreieck(4.0, 3.0);
+        dreieckUndKreise[6] = new Dreieck(2.0, 1.0);
+        dreieckUndKreise[7] = new Dreieck(5.0, 8.0);
+        dreieckUndKreise[8] = new Dreieck(2.0, 7.2);
+        dreieckUndKreise[9] = new Dreieck(14.0, 3.8);
+
         double gesamtflaeche = 0.0;
 
-        for (Kreis kreis : kreise) {
-            gesamtflaeche += kreis.flaeche();
+        for (Figur figur : dreieckUndKreise) {
+            gesamtflaeche += figur.flaeche();
         }
 
-        for (Dreieck dreieck : dreiecke) {
-            gesamtflaeche += dreieck.flaeche();
-        }
-        
-        System.out.printf("Die Fläche beträgt: %.2f", gesamtflaeche);        
+//        for (Dreieck dreieck : dreieckUndKreise) {
+//            gesamtflaeche += dreieck.flaeche();
+//        }
+
+        System.out.printf("Die Fläche beträgt: %.2f", gesamtflaeche);
     }
 }
