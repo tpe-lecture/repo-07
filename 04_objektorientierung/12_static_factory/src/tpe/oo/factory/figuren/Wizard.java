6141b13c33aa013eb722ab78482c1a9c7bc1b0f2
David <coregaer89@hotmail.de>
2016-11-07 11:32:08 +0100

    Bestes Game ever!

--- 04_objektorientierung/12_static_factory/src/tpe/oo/factory/figuren/Wizard.java
+++ 04_objektorientierung/12_static_factory/src/tpe/oo/factory/figuren/Wizard.java
@@ -6,14 +6,14 @@ import de.smits_net.games.framework.image.AnimatedImage;
 /**
  * Zauberer.
  */
-public class Wizard extends Figur {
-    
+class Wizard extends Figur {
+
     /**
      * Neuer Figur anlegen.
      *
      * @param board das Spielfeld
      */
-    public Wizard(Board board) {
+    Wizard(Board board) {
         super(board, new AnimatedImage(50, 8, "assets/wizard.png"));
     }
 }
