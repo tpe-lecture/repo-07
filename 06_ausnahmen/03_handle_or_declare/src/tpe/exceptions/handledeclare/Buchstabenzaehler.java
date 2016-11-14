c2fbc354fffe97c282436555e7a716a1363ef3af
David <coregaer89@hotmail.de>
2016-11-14 15:48:59 +0100

    works

--- 06_ausnahmen/03_handle_or_declare/src/tpe/exceptions/handledeclare/Buchstabenzaehler.java
+++ 06_ausnahmen/03_handle_or_declare/src/tpe/exceptions/handledeclare/Buchstabenzaehler.java
@@ -1,10 +1,10 @@
 package tpe.exceptions.handledeclare;
 
-//import java.io.BufferedReader;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Anwendung, die die Buchstabenhäufigkeit in einem Text analysiert.
@@ -18,7 +18,7 @@ public class Buchstabenzaehler {
      */
     public static void main(String[] args) {
         Buchstabenzaehler bs = new Buchstabenzaehler();
-//        bs.run();
+        bs.run();
 
     }
 
@@ -29,15 +29,29 @@ public class Buchstabenzaehler {
      *      Buchstabenhäufigkeit.
      */
     private void run() {
-//        System.out.print("Bitte geben Sie den Dateinamen an: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String dateiname = scanner.nextLine();
-//
-//        int[] statistik;
-//
-//        statistik = parseFile("assets/" + dateiname);
-//        printStatistik(statistik);
-//        scanner.close();
+            System.out.print("Bitte geben Sie den Dateinamen an: ");
+            Scanner scanner = new Scanner(System.in);
+            String dateiname = scanner.nextLine();
+
+            int[] statistik;
+
+            try {
+                statistik = parseFile("assets/" + dateiname);
+                printStatistik(statistik);
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("Datei nicht gefunden.");
+            }
+            catch (IOException e) {
+                System.out.println("Something strange happened.");
+            }
+            catch (StatistikException e) {
+                System.out.println("Statistik fehlerhaft.");
+            }
+
+
+
+            scanner.close();
     }
 
     /**
@@ -46,29 +60,30 @@ public class Buchstabenzaehler {
      * @param statistik Statistik
      * @throws StatistikException Fehler bei den Eingabedaten
      */
-    private void printStatistik(int[] statistik) {
+    private void printStatistik(int[] statistik)
+        throws StatistikException {
 
-//        int summe = 0;
-//
-//        for (int haeufigkeit : statistik) {
-//            summe += haeufigkeit;
-//        }
-//
-//        double prozentSumme = 0.0;
-//
-//        for (char c = 'a'; c <= 'z'; c++) {
-//            int anzahl = statistik[c - 'a'];
-//            double prozent = (double) anzahl / (double) summe * 100;
-//            System.out.printf("%s: %.2f%% %n", "" + c, prozent);
-//
-//            prozentSumme += prozent;
-//        }
-//
-//        if ((prozentSumme < 99.0) || (prozentSumme > 101.0)) {
-//            throw new StatistikException(Double.toString(prozentSumme));
-//        }
-//
-//        System.out.printf("Summe: %.2f%% %n", prozentSumme);
+        int summe = 0;
+
+            for (int haeufigkeit : statistik) {
+                summe += haeufigkeit;
+            }
+
+        double prozentSumme = 0.0;
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            int anzahl = statistik[c - 'a'];
+            double prozent = (double) anzahl / (double) summe * 100;
+            System.out.printf("%s: %.2f%% %n", "" + c, prozent);
+
+            prozentSumme += prozent;
+        }
+
+        if ((prozentSumme < 99.0) || (prozentSumme > 101.0)) {
+            throw new StatistikException(Double.toString(prozentSumme));
+        }
+
+        System.out.printf("Summe: %.2f%% %n", prozentSumme);
     }
 
     /**
@@ -80,25 +95,29 @@ public class Buchstabenzaehler {
      * @throws IOException generelles IO-Problem
      * @throws FileNotFoundException Datei gibt es nicht
      */
-    private int[] parseFile(String filename) {
+    private int[] parseFile(String filename)
+            throws IOException, FileNotFoundException {
 
         int[] statistik = new int['z' - 'a' + 1];
-//
-//        BufferedReader br = new BufferedReader(new FileReader(filename));
-//
-//        String line;
-//
-//        while ((line = br.readLine()) != null) {
-//            char[] chars = line.toLowerCase().toCharArray();
-//
-//            for (char c : chars) {
-//                if (('a' <= c) && (c <= 'z')) {
-//                    statistik[c - 'a']++;
-//                }
-//            }
-//        }
-//
-//        br.close();
+
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+
+        String line;
+
+            while ((line = br.readLine()) != null) {
+                char[] chars = line.toLowerCase().toCharArray();
+
+                for (char c : chars) {
+                    if (('a' <= c) && (c <= 'z')) {
+                        statistik[c - 'a']++;
+                    }
+                }
+            }
+
+
+
+
+        br.close();
 
         return statistik;
     }
