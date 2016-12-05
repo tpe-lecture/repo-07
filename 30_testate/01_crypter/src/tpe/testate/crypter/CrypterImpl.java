16598746a985dbb4f773a5c19274ae496347bf2b
David <coregaer89@hotmail.de>
2016-12-05 13:39:43 +0100

    Unzulässige Zeichen und daraus resultierende '\u0000'-Zeichen werden jetzt stillschweigend rausgefiltert.

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,15 +1,31 @@
 package tpe.testate.crypter;
-
+/**
+ * Diese Klasse ver- und entschlüsselt Strings mit vorgegebenem Schlüssel.
+ * @author Asus
+ *
+ */
 public class CrypterImpl implements Crypter {
-
+	/**
+	 * Das originale Zeichenalphabeth.
+	 */
 	static String a = "abcdefghijklmnopqrstuvwxyz0123456789";
+	/**
+	 * Das verschlüsselte Alphabeth
+	 */
 	static String b = "4bcd3fghijk1mn0pqrs7uvwxyzol2ea56t89";
 
+	/**
+	 * Methode verschlüsselt den übergebenen String und gibt neuen zurück.
+	 * @param input Der übergebene String
+	 */
 	@Override
 	public String encrypt(String input) {
 		char[] result = new char[input.length()];
 		input = input.toLowerCase();
-		for (int i = 0, j = 0; i < input.length(); i++) {
+		int j = 0;
+
+		//Verschlüsselt zu verschüsselnde Zeichen
+		for (int i = 0; i < input.length(); i++) {
 			char temp = input.charAt(i);
 			int x = a.indexOf(temp);
 			if(x >= 0) {
@@ -21,12 +37,25 @@ public class CrypterImpl implements Crypter {
 				j++;
 			}
 		}
-		return new String(result);
-	}
 
+		//Array ohne endenden null-Zellen
+		char[] arrayWhichGetsReturned = new char[j];
+		int i = 0;
+		for(char temp : result) {
+			arrayWhichGetsReturned[i++] = temp;
+		}
+		return new String(arrayWhichGetsReturned);
+	}
+	/**
+	 * Methode entschlüsselt den übergebenen String und gibt neuen zurück.
+	 * @param input Der übergebene String
+	 */
 	@Override
 	public String decrypt(String input) throws IllegalArgumentException {
+
 		char[] result = new char[input.length()];
+
+		//Ersetze verschlüsselte Zeichen
 		for (int i = 0; i < input.length(); i++) {
 			char temp = input.charAt(i);
 			int x = b.indexOf(temp);
