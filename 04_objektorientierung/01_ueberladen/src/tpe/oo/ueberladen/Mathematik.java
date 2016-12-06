c2d932c3d41c81bf672c2636c96dcb3909ba1b2f
David <coregaer89@hotmail.de>
2016-12-06 18:47:02 +0100

    Musterlösung! Beste was gibt!

--- 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
+++ 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
@@ -8,8 +8,43 @@ public final class Mathematik {
     /**
      * Konstruktor.
      */
-    private Mathematik() {        
+    private Mathematik() {
     }
-    
+    /**
+     * Liefert das Maximum zwischen zwei Zahlen.
+     * @param firstValue Erste Zahl
+     * @param secondValue Zweite Zahl
+     * @return Erste Zahl bei Gleichheit, ansonsten Maximum
+     */
+    public static int max(int firstValue, int secondValue) {
+        return (firstValue >= secondValue ? firstValue : secondValue);
+    }
+
+    /**
+     * Liefert das Maximum dreier Zahlen.
+     * @param firstValue Erste Zahl
+     * @param secondValue Zweite Zahl
+     * @param thirdValue Dritte Zahl
+     * @return Bei Gleichheit entweder Erste oder Zweite, ansonsten Maximum
+     */
+    public static int max(int firstValue, int secondValue, int thirdValue) {
+        int firstOrSecondMax = max(firstValue, secondValue);
+        return (firstOrSecondMax >= thirdValue ? firstOrSecondMax : thirdValue);
+    }
+
+    /**
+     * Liefert das Maximum von vier Zahlen.
+     * @param firstValue Erste Zahl
+     * @param secondValue Zweite Zahl
+     * @param thirdValue Dritte Zahl
+     * @param fourthValue Vierte Zahl
+     * @return Bei Gleichheit das Maximum der ersten drei, ansonsten Maximum
+     */
+    public static int max(int firstValue, int secondValue,
+            int thirdValue, int fourthValue) {
+        int firstThreeMaximum = max(firstValue, secondValue, thirdValue);
+        return (firstThreeMaximum >= fourthValue
+                ? firstThreeMaximum : fourthValue);
+    }
     // Bitte fügen Sie Ihre Methoden hier ein
 }
